da09c9e0e006eeb00b98910c937bbf729165cf54
kaliesboopathy333 <kaliesboopathy@gmail.com>
2026-03-17 18:21:14 +0530

    fix app.run

--- dashboard.py
+++ dashboard.py
@@ -202,4 +202,4 @@ def data():
 # 🔥 START SERVER
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
-    app.run_server(host='0.0.0.0', port=port, debug=False)
+   app.run(host='0.0.0.0', port=port, debug=False)
